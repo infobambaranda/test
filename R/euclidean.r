a5f85aa2f07da565749f4de54e107c4a92576c84
infobambaranda <info.bambaranda@gmail.com>
2022-09-11 18:59:26 +0200

    euclidean function is added to the package with roxygen documentation

--- R/euclidean.r
+++ R/euclidean.r
@@ -1,4 +1,14 @@
-#1.1.1
+#' Euclidean
+#'
+#' @param a A numeric scalar
+#' @param b A numeric scalar
+#'
+#' @return Return the greatest common divisor of the parameters a and b
+#' @export
+#'
+#' @examples euclidean(123612, 13892347912)
+#' euclidean(13892347912,123612)
+#' euclidean(100, 1000)
 euclidean <- function(a,b){
   while (b != 0){
     temp <- b
