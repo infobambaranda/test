759bb452539c41206b6458d1016d3e2285629285
infobambaranda <info.bambaranda@gmail.com>
2022-09-14 06:15:29 +0200

    dataset was added with documentation

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#'Wiki_graph
+#'
+#'A dataset containg nodes and edge details of the a pgraph
+#'@format A dataframe with 3 variables and 18 rows:
+#'\describe{
+#'  \item{v1}{Node 1 of al edges}
+#'  \item{V2}{Node 2 of all edges}
+#'  \item{w}{Node 3 of all edges}
+#'}
+"wiki_graph"
